c1abbeae4d1f8d2dc6594b98baa2682e887e69af
GitLabBR <gitlabbrasil@proton.me>
2024-11-20 15:55:32 -0300

    New update

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 import setuptools
 from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 
 setup (
     name="gitverbose",
     description="Easy clone github repo, Internet files.",
-    long_description="Make your life easier while cloning github repo.",
-    version="0.3",
+    long_description=long_description,
+    version="0.5",
     readme='README.md',
     author="caique9014",
+    license="LICENSE.txt",
     author_email="caiqueonz777@proton.me",
+    url = "https://github.com/GitLabBR/gitverbose",
     packages=['gv'],
     entry_points={
         'console_scripts': ['gv=gv.entry:gv_entry_point']
@@ -30,6 +36,7 @@ setup (
     install_requires=[
         "requests",
         "argparse",
+        "pathlib",
         "git-clone"
     ]
 )
